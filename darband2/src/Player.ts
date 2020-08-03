7de28678ac00daa366457f31215e5b8b745771cc
Sergo Pasoevi <spasoevi@gmail.com>
2020-08-03 19:57:34 +0400

    Player movement restricted by blocking objects

--- darband2/src/Player.ts
+++ darband2/src/Player.ts
@@ -1,6 +1,10 @@
-import { Actor } from "./Actor";
+import { Actor, ActorTemplate } from "./Actor";
 
 export class Player extends Actor {
+    constructor(spec: ActorTemplate) {
+        super({ ...spec, name: "You", char: "@", sprite: 0 });
+    }
+
     public tryMove(dx: number, dy: number): boolean {
         if (super.tryMove(dx, dy)) {
             this.game.tick();

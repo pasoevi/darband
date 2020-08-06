301a2d97ae4920a4beca1cb0003e2e8217dcd0b1
Sergi Pasoev <s.pasoev@gmail.com>
2020-08-07 02:16:31 +0400

    Fix naming conflicts

--- src/item.ts
+++
@@ -1,8 +0,0 @@
-import { Actor } from "./Actor";
-import { Tile } from "./Tile";
-
-export class Item extends Actor {
-    public constructor(name: string, sprite: number, tile: Tile) {
-        super(name, tile, sprite);
-    }
-}

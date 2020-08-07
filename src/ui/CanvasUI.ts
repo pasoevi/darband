4b03b8f8861b0d9bbdd6950061660a3409f9fd75
Sergo Pasoevi <spasoevi@gmail.com>
2020-08-07 14:22:05 +0400

    Add HTML game ui placeholder

--- src/ui/CanvasUI.ts
+++ src/ui/CanvasUI.ts
@@ -6,10 +6,36 @@ export class CanvasUI implements GameUI {
     public msg(game: Game, text: string, color?: string): void {
         // eslint-disable-next-line no-console
         console.log(text);
-        game.renderer.drawText("Level: " + game.levelID, 25, false, 40, "violet");
+        game.renderer.drawText(
+            "Level: " + game.levelID,
+            25,
+            false,
+            40,
+            "violet",
+        );
     }
 
     public render(game: Game): void {
-        game.renderer.drawText("Level: " + game.levelID, 25, false, 40, "violet");
+        game.renderer.drawText(
+            "Level: " + game.levelID,
+            25,
+            false,
+            40,
+            "violet",
+        );
+    }
+
+    public renderTitleScreen(game: Game): void {
+        game.renderer.drawRect("rgba(0,0,0,.75)", 0, 0);
+        game.gameState = "TITLE";
+
+        // this.renderer.drawText("SUPER", 40, true, canvas.height / 2 - 110, "white");
+        game.renderer.drawText(
+            "PRESS ANY KEY TO START",
+            50,
+            true,
+            500,
+            "white",
+        );
     }
 }
